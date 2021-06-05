f5aaa544f08ac7e0f8cd67acd8bc69af4f799f9e
topistalamaro <82517366+topistalamaro@users.noreply.github.com>
2021-06-05 07:53:20 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+let calBtn = document.getElementById("btn");
+calBtn.addEventListener("click", ()=>{BMI();});
+function BMI(){
+  
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+    const bmi = Math.round(weight / (height ** 2));
+
+    
+    if (bmi < 18.5) {
+       document.getElementById('result').innerHTML = "Your BMI is :"+  bmi +  ", so you are underweight.";
+
+    } else if ((bmi >= 18.5) && (bmi <= 24.9)) {
+      document.getElementById('result').innerHTML = "Your BMI is :"+  bmi +  ", so you are normal weight.";
+
+
+
+    } else if ((bmi > 24.9) && (bmi <= 29.9)) {
+      document.getElementById('result').innerHTML = "Your BMI is :"+  bmi +  ", so you are overweight.";
+
+
+    } else if ((bmi > 29.9) && (bmi <= 35)) {
+      document.getElementById('result').innerHTML = "Your BMI is :"+  bmi +  ", so you are obese.";
+
+    } else {
+      document.getElementById('result').innerHTML = "Your BMI is :"+  bmi +  ",s so you are extremely obese.";
+
+    }
+    
+};
+
